8fe6056248e7f4ec3f31677fe502cb47ca096cdb
Urszula Kuczmińska <ukuczminska@hso.grupa.skok>
2018-04-29 17:48:57 +0200

    garnitury.html dodanie modalu, zmiana responsywności i kolorów buttonów

--- src/js/myJS.js
+++ src/js/myJS.js
@@ -23,3 +23,15 @@ function openDescr(evt, contentName) {
     evt.currentTarget.className += " active";
 }
 
+
+// do modalu:
+$('#fitting').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var rodzaj = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  // modal.find('.modal-title').text('New message to ' + rodzaj)
+  modal.find('#rodzaj').val(rodzaj)
+})
+
