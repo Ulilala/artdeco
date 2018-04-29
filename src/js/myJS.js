0e8feff4b2fe7891b0208d3557cb311ccf4f9983
Urszula Kuczmińska <ukuczminska@hso.grupa.skok>
2018-04-29 11:09:43 +0200

    aktualizacja garnitury.html; ucieka stopka; zawartość za szybko przechodzi w jedną kolumnę

--- src/js/myJS.js
+++ src/js/myJS.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
     });
 });
 
+
+// strona garnitury menu ze zdjęć photolinks
+function openDescr(evt, contentName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(contentName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
